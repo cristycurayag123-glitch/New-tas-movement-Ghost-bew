57a3d75f09155fc5eb6807ec811cd9e3482ef16b
cristycurayag123-glitch <cristycurayag123@gmail.com>
2026-03-01 18:46:16 +0800

    Yes tas

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("YOUR_RAW_URL_HERE"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cristycurayag123-glitch/New-tas-movement-Ghost-bew/refs/heads/main/script.lua)")()
